52fcc3a65c5bfaf4557e36951ca049ce95dedbd9
pobidzinski <pawel.obidzinski@gmail.com>
2018-05-28 14:23:44 +0200

    m

--- blog_project/blog_project/blog/views.py
+++ blog_project/blog_project/blog/views.py
@@ -1,3 +1,63 @@
 from django.shortcuts import render
+from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
+from blog.models import Post, Comment
+from blog.forms import PostForm, CommentForm
+from django.urls import reverse_lazy
 
-# Create your views here.
+# Import mixins
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
+
+class AboutView(TemplateView):
+    template_name = 'about.html'
+
+
+class PostListView(ListView):
+    model = Post
+
+    # Create generic view. 
+    # With get_queryset you are doing query on model (choosen)
+    # Grap postm model, all object, and filter out based on conditions.
+    # __lte is conditions (less than or equat to)
+    # order by published_date (- means descending order)
+    def get_queryset(self):
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+# Add mixing for login required to create post
+class CreatePostView(LoginRequiredMixin, CreateView):
+    model = Post
+
+    # required for mixing
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+
+    # required for mixing
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_detail.html'
+    form_class = PostForm
+
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = rreverse_lazy('post_list')        # you want to activate when actual delete, hence laze reverse
+
+
+class DraftListView(LoginRequiredMixin, ListView):
+    model = Post
+
+    login_url = '/login/'
+    redirect_field_name = 'blog/post_list.html'
+
+    def get_queryset(self):
+        return Post.objects.filter(published_date__isnull=True).order_by('created_date')
--- blog_project/blog_project/blog_project/urls.py
+++ blog_project/blog_project/blog_project/urls.py
@@ -9,13 +9,5 @@ from django.conf.urls import include, url
 # admin.autodiscover()
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', blog_project.views.home, name='home'),
-    # url(r'^blog_project/', include('blog_project.blog_project.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
