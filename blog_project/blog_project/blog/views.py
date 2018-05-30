f035ca6d55195b58970444b1d0bc690809b0939e
pobidzinski <pawel.obidzinski@gmail.com>
2018-05-30 14:03:05 +0200

    dupa

--- blog_project/blog_project/blog/views.py
+++ blog_project/blog_project/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from django.views.generic import (TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView)
 from blog.models import Post, Comment
 from blog.forms import PostForm, CommentForm
@@ -50,7 +51,7 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
 
 class PostDeleteView(LoginRequiredMixin, DeleteView):
     model = Post
-    success_url = rreverse_lazy('post_list')        # you want to activate when actual delete, hence laze reverse
+    success_url = reverse_lazy('post_list')        # you want to activate when actual delete, hence laze reverse
 
 
 class DraftListView(LoginRequiredMixin, ListView):
@@ -61,3 +62,49 @@ class DraftListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('created_date')
+
+
+############# COMMENTS #################
+
+@login_required     # make view required by login
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)       # get post or 404 from Post model, where PK is similar
+
+    if request.method == 'POST':        # if someone filled form
+        form = CommentForm(request.POST)
+
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+
+    else:
+        form = CommentForm()
+
+    return render(request, 'blog/comment_form.html', {'form' : form})
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+
+    return redirect('post_detail', pk=comment.post.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    
+    return redirect('post_detail', pk=post_pk)
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+
+    return redirect('post_detail', pk=pk)
