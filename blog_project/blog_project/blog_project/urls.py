9cdc097ff714e7433afa172fab4240f2c176d1dc
pobidzinski <pawel.obidzinski@gmail.com>
2018-05-30 16:43:38 +0200

    master

--- blog_project/blog_project/blog_project/urls.py
+++ blog_project/blog_project/blog_project/urls.py
@@ -3,11 +3,15 @@ Definition of urls for blog_project.
 """
 
 from django.conf.urls import include, url
+from django.contrib.auth import views
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     url(r'', include('blog.urls')),
+    url(r'admin/', admin.site.urls),
+    url(r'accounts/login/$', views.login, name='login'),
+    url(r'account/logout/$', views.logout, name='logout', kwargs={ 'next_page' : '/' }),
 ]
